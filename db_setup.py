54587e97fd58243f6c5b6c6bffa4e703dcc8490d
Shreyansh Pathak <shreyanshp132@gmail.com>
2026-09-22 18:42:11 +0530

    feat: add db_setup.py to initialize PostgreSQL schema for stock prices

---
+++ db_setup.py
@@ -0,0 +1,19 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+db_url=os.getenv("DATABASE_URL")
+conn= psycopg2.connect(db_url)
+cursor=conn.cursor()
+
+
+create_table_query="""
+CREATE TABLE IF NOT EXISTS
+daily_stock_price(symbol VARCHAR(10) NOT NULL,trade_date DATE NOT NULL,open_price FLOAT,high_price FLOAT,low_price FLOAT,close_price FLOAT,volume BIGINT,PRIMARY KEY (symbol, trade_date));
+"""
+cursor.execute(create_table_query)
+conn.commit()
+cursor.close()
+conn.close()
+print("table created successfully")
